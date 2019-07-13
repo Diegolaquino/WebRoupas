e268dea96ac7ce8b9d8bcc347ba29c081ab59f56
Diego Aquino <diegol.aquino@gmail.com>
2019-07-13 16:29:08 -0300

    Initial commit

---
+++ WebRoupas/Models/BancoDeDados.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebRoupas.Models
+{
+    /*
+     Essa class representa o banco de dados     
+    */
+    public class BancoDeDados : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+
+        public BancoDeDados(DbContextOptions<BancoDeDados> options) : base(options) { }
+
+        // Esse Dbset representa a tabela da class no banco de dados, ou seja, para cada tabela, haverá um Dbset que deve ser criado
+        //aqui.
+        public DbSet<Funcionario> Funcionarios { get; set; }
+    }
+
+}
---
+++ WebRoupas/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using WebRoupas.Models;
+
+namespace WebRoupas
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            //Esse método configura o nome da conexão com o banco de dados, pode reparar que DefaultConnection é uma espécie de variável,
+            //pois ela está definida no arquivo appsettings.json. Neste arquivo você verifica o nome da connectionstring. No seu pc a connectionstring 
+            //será diferente, bastando você alterar o valor da "variável" defaultConnection para a sua connectionstring, ou seja, não precisa mexer nessa
+            // configuração abaixo.
+            services.AddDbContext<BancoDeDados>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
